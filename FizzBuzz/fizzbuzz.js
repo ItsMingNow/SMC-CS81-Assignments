b3abc879cf7db7cf10bc7c11dd6fa56a64d160c1
Yanming Yu <69600884+jimmyjameswong@users.noreply.github.com>
2021-03-16 16:21:57 -0700

    fizzbuzz assignment

---
+++ FizzBuzz/fizzbuzz.js
@@ -0,0 +1,22 @@
+
+// Loop 1 to 10
+for(let i = 1; i<11;i++){
+ if(i % 2 == 1){
+   console.log("fizz");
+ }
+ else {
+   console.log(i);
+ }
+  // odd number print fizz
+};
+
+// loop 10 to 1
+for(let i = 10; i>0;i--){
+  if(i % 2 == 1){
+    console.log("buzz");
+  }
+  else {
+    console.log(i);
+  }
+  // even number print buzz
+};
