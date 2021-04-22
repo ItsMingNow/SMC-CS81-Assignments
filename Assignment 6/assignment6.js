319d6b416fe8bb0af9dc1c2cf4848b0106b81618
Yanming Yu <69600884+jimmyjameswong@users.noreply.github.com>
2021-04-22 00:29:44 -0700

    assignment 6 update

--- Assignment 6/assignment6.js
+++ Assignment 6/assignment6.js
@@ -1,30 +1,24 @@
-class Monster{
-    constructor(language){
-        this.language = language;
-        this.stomach = [];
-    }
-
-    eat(food_item){
-
+function Monster(language){
+    this.language = language;
+    this.stomach = []
+    this.eat = function (food_item){
         this.stomach.push(food_item);
         return this.stomach;
     }
-
-    speak(sentence){
+    this.speak = function(sentence){
         return sentence;
     }
 }
 
 const monsterTruck = new Monster("truck");
 
-let Gremlins = new Monster('Gremlinese');
+let gremlin = new Monster('Gremlinese');
 
-console.log(monsterTruck);
 console.log(monsterTruck.eat("gasoline"));
 console.log(monsterTruck.eat("nails"));
 console.log(monsterTruck.speak("vroom vroom"));
 
-Gremlins.speak = function(sentence){
+gremlin.speak = function(sentence){
     let gremlinese = []
         for(let i = 0; i<sentence.split(' ').length; i++){
             gremlinese.push("gar ");
@@ -33,4 +27,26 @@ Gremlins.speak = function(sentence){
         return string;
 }
 
-console.log(Gremlins.speak("I like water"));
+console.log(gremlin.speak("I like water"));
+
+function Gremlin(){
+    Monster.apply(this, arguments);
+    this.speak = function(sentence){
+        let gremlinese = []
+            for(let i = 0; i<sentence.split(' ').length; i++){
+                gremlinese.push("gar ");
+            }
+            let string = gremlinese.join('');
+            return string;
+    }
+} 
+
+let gremlin2 = new Gremlin('english');
+
+console.log(gremlin2.eat('garbage'));
+console.log(gremlin2.eat('more garbage'));
+
+let gremlin3 = new Gremlin('english');
+
+console.log(gremlin3.eat('recycled garbage'));
+console.log(gremlin3.eat('more recycled garbage'));
